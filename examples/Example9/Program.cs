714b8792b4ca30bbb9462463eeb408e78d42909e
Ashley Davis <ashley@codecapers.com.au>
2014-12-23 14:47:11 +1000

    Added an example for automatically dependency resolution.

---
+++ examples/Example9/Program.cs
@@ -0,0 +1,65 @@
+﻿using Logger;
+using RSG.Factory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+//
+// This example shows how to inject property dependencies into a factory created object.
+// 
+namespace Example
+{
+    public interface IMyDependency
+    {
+        string Message { get; }
+    }
+
+    [FactoryCreatable(typeof(IMyDependency))] // With this attribute the depenency will now be automatically resolved.
+    public class MyDependency : IMyDependency
+    {
+        public string Message
+        {
+            get
+            {
+                return "My auto resolved dependency!";
+            }
+        }
+    }
+
+    public interface IMyType
+    {
+        string Message { get; }
+    }
+
+    [FactoryCreatable(typeof(IMyType))]
+    public class MyType : IMyType
+    {
+        [Dependency]
+        public IMyDependency MyDependency { get; set; }
+
+        public string Message
+        {
+            get
+            {
+                return MyDependency.Message;
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new Factory("MyApp", new MyLogger());
+
+            // Auto register types and dependencies.
+            factory.AutoRegisterTypes();
+
+            // Create an instance with dependency injected.
+            var myFactoryCreatedObject = factory.CreateInterface<IMyType>();
+
+            Console.WriteLine(myFactoryCreatedObject.Message);
+        }
+    }
+}
